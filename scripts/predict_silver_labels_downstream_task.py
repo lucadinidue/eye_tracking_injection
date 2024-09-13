2048445226ca4dd4ddd7907c0bf03137c707824b
lucadinidue <luca.dini16@gmail.com>
2024-09-13 15:58:04 +0200

    added prediction of silver labels

---
+++ scripts/predict_silver_labels_downstream_task.py
@@ -0,0 +1,76 @@
+import sys
+import os
+sys.path.append(os.path.abspath('.'))
+
+from modules.data.dataset_utils import create_senteces_from_data, scale_datasets, tokenize_and_align_labels
+from modules.modeling.custom_modeling_roberta import RobertaForMultiTaskTokenClassification
+from modules.modeling.custom_data_collator import DataCollatorForMultiTaskTokenClassification
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+from datasets import Dataset
+from torch.nn import MSELoss
+from tqdm import tqdm
+import pandas as pd
+import argparse
+import torch
+
+TASKS = ['firstfix_dur','dur','firstrun_nfix','nfix','firstrun_dur']
+
+
+def load_complexity_dataset(src_path:str) -> Dataset:
+    df = pd.read_csv(src_path)
+    annotators_columns = [col for col in df.columns if col.startswith('judgement')]
+    df['label'] = df[annotators_columns].mean(axis=1)
+    df = df[['SENTENCE', 'label']]
+    df = df.rename(columns={'SENTENCE': 'text'})
+    return Dataset.from_pandas(df)
+
+
+def add_dummy_eye_gaze_features(example):
+    for task in TASKS:
+        example[f'label_{task}'] = [0]*len(example['input_ids'])
+    return example
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--user_id', dest='user_id', type=int)
+    parser.add_argument('-t', '--downstream_task', dest='downstream_task', type=str, choices=['complexity', 'sentiment'])
+    args = parser.parse_args()
+
+    model_path = f'models/eye_gaze_finetuning/average_loss/50epochs_lr1e-05/roberta-base_pp{args.user_id}'
+    output_path = f'data/silver_labels/{args.downstream_task}/{args.user_id}.tsv'
+
+
+    if args.downstream_task == 'complexity':
+        train_path = 'data/complexity/complexity_ds_en_train.csv'
+        train_dataset = load_complexity_dataset(train_path)
+    else:
+        raise NotImplementedError(f'Downstream task "{args.downstream_task}" not implemented yet.')
+    
+    model = RobertaForMultiTaskTokenClassification.from_pretrained(model_path)
+    tokenizer = AutoTokenizer.from_pretrained('FacebookAI/roberta-base', add_prefix_space=True)
+
+    def preprocess_function(examples):
+        return tokenizer(examples['text'], padding=True, truncation=True)
+
+    tokenized_train_dataset = train_dataset.map(preprocess_function, remove_columns=['text'], batched=True)
+    data_collator = DataCollatorForMultiTaskTokenClassification(tokenizer=tokenizer)
+    tokenized_train_dataset = tokenized_train_dataset.map(add_dummy_eye_gaze_features)
+    train_dataloader = DataLoader(tokenized_train_dataset, shuffle=False, collate_fn=data_collator, batch_size=1)
+
+    model.eval()
+    dataset_features = {task:[] for task in TASKS}
+    with torch.no_grad():
+        for el in tqdm(train_dataloader):
+            logits = model(**el)['logits']
+            for task in TASKS:
+                dataset_features[task].append(logits[task].squeeze().tolist())
+    
+    features_df = pd.DataFrame.from_dict(dataset_features)
+    features_df.to_csv(output_path, sep='\t')
+
+
+
+if __name__ == '__main__':
+    main()
